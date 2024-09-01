a662efb1789ffe3d8d2f6eb54539b0a501d1d8b2
Mayur Dabade <113239727+mayurd8862@users.noreply.github.com>
2024-09-01 15:01:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="filesense",
-    version="v0.1",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "langchain_community",
